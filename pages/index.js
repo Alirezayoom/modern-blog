de138d844e663d1ecbc9315e055cfb29441d6c69
alireza <alirezayoom@gmail.com>
2022-11-12 09:20:59 +0430

    services added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
 const posts = [
   { title: "React Testing", excerpt: "Learn React Testing" },
---
+++ services/index.js
@@ -0,0 +1,46 @@
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query Assets {
+      assets {
+        createdAt
+        id
+        publishedAt
+        fileName
+        url
+        updatedAt
+      }
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              id
+              name
+              photo {
+                url
+              }
+            }
+            createdAt
+            excerpt
+            slug
+            title
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.postConnection.edges;
+};
