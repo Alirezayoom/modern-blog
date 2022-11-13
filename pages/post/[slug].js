049ab102a1060e2e9eec62facda81c7536b3daad
alireza <alirezayoom@gmail.com>
2022-11-13 10:41:12 +0430

    author,comments,commentsForm & postDetail created

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -7,17 +7,17 @@ import {
   PostWidget,
   Author,
   Comments,
-  CommentsFrom,
+  CommentsForm,
 } from "../../components";
 
 const PostDetails = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="grid grid-cols-1 lg:grid-cosl-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetail />
           <Author />
-          <CommentsFrom />
+          <CommentsForm />
           <Comments />
         </div>
         <div className="col-span-1 lg:col-span-4">
