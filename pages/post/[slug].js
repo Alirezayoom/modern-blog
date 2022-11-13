9b7a16d80f92136bcfdb67887b416910b1bc383c
alireza <alirezayoom@gmail.com>
2022-11-13 10:11:24 +0430

    post added

---
+++ pages/post/[slug].js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { getPosts, getPostDetails } from "../../services";
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsFrom,
+} from "../../components";
+
+const PostDetails = () => {
+  return <div>PostDetails</div>;
+};
+
+export default PostDetails;
