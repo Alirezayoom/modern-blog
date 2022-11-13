95c665e9049b15ddd307ff5ef3e31db0da83dd09
alireza <alirezayoom@gmail.com>
2022-11-13 10:16:57 +0430

    slug added in post dir

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -11,7 +11,24 @@ import {
 } from "../../components";
 
 const PostDetails = () => {
-  return <div>PostDetails</div>;
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cosl-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          <PostDetail />
+          <Author />
+          <CommentsFrom />
+          <Comments />
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostDetails;
