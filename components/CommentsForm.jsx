dc91aa45054db55cd73323ceadec4b44bd20a8be
alireza <alirezayoom@gmail.com>
2022-11-16 09:55:30 +0430

    logic added to comments

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 
+import { submitComment } from "../services";
+
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocaleStorage] = useState(null);
@@ -8,6 +10,11 @@ const CommentsForm = ({ slug }) => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name");
+    emailEl.current.value = window.localStorage.getItem("email");
+  });
 
   const commentSubmitHandler = () => {
     setError(false);
@@ -25,17 +32,27 @@ const CommentsForm = ({ slug }) => {
     const commentObj = { name, email, comment, slug };
 
     if (storeData) {
-      localStorage.setItem("name", name);
-      localStorage.setItem(email, email);
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem(email, email);
     } else {
-      localStorage.removeItem("name", name);
-      localStorage.removeItem("email", email);
+      window.localStorage.removeItem("name", name);
+      window.localStorage.removeItem("email", email);
     }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true);
+
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    });
   };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Commentsform</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Leave a Reply
+      </h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
           ref={commentEl}
--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -101,7 +101,6 @@ const PostDetail = ({ post }) => {
         </div>
 
         <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-        {console.log(post.content.raw)}
         {post.content.raw.children.map((typeObj, index) => {
           const children = typeObj.children.map((item, itemIndex) =>
             getContentFragment(itemIndex, item.text, item)
