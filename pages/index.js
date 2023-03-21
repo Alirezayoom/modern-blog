12e82e2dc2d884a375a4cc52dff671e1380d1b81
alirezayoom <alirezayoom@gmail.com>
2023-03-21 18:04:19 +0430

    dummy data added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,51 @@
+import Head from "next/head";
+
+const data = [
+  {
+    id: 1,
+    title: "React Best Practices",
+    excerpt: "Learn React best practices in one topic",
+  },
+  { id: 2, title: "React Testing", excerpt: "Learn React testing easily" },
+  {
+    id: 3,
+    title: "React Best Practices",
+    excerpt: "Learn React best practices in one topic",
+  },
+  { id: 4, title: "React Testing", excerpt: "Learn React testing easily" },
+];
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div
+      style={{
+        maxWidth: "80rem",
+        margin: "0 auto",
+        padding: "0 2rem",
+        marginBottom: "2rem",
+      }}
+    >
+      <Head>
+        <title>Modern Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div
+        style={{
+          backgroundColor: "red",
+          display: "grid",
+          gridTemplateColumns: "repeat(4,1fr)",
+        }}
+      >
+        <div style={{ gridColumnStart: "1", gridColumnEnd: "4" }}>
+          {data.map((data) => (
+            <div key={data.id}>
+              {data.title}
+              {data.excerpt}
+            </div>
+          ))}
+        </div>
+        <div style={{ backgroundColor: "green" }}>hi</div>
+      </div>
+    </div>
+  );
 }
