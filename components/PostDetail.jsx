bf4b3d3951c55451384bdee041cf82f84fb48312
alirezayoom <alirezayoom@gmail.com>
2023-03-24 22:34:36 +0430

    style added

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,5 +1,11 @@
 const PostDetail = () => {
-  return <div>PostDetail</div>;
+  return (
+    <div
+      style={{ backgroundColor: "#333", padding: "1rem", borderRadius: "1rem" }}
+    >
+      PostDetail
+    </div>
+  );
 };
 
 export default PostDetail;
