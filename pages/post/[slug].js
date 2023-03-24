60ba529c2c598ccb7e859f4dc43cb117dd2b4331
alirezayoom <alirezayoom@gmail.com>
2023-03-24 22:33:45 +0430

    ssg added

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,13 +9,63 @@ import {
   CommentsForm,
 } from "../../components";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
-    <div>
-      <PostDetail />
-      <Author />
-      <CommentsForm />
-      <Comments />
+    <div
+      style={{
+        maxWidth: "64rem",
+        margin: "2rem auto",
+        padding: "0 2rem",
+      }}
+    >
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "8fr 3fr",
+          gap: "1rem",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            gap: "1rem",
+          }}
+        >
+          <PostDetail post={post} />
+          <Author author={post.author} />
+          <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} />
+        </div>
+        <div
+          style={{
+            position: "sticky",
+            top: "1rem",
+            alignSelf: "start",
+          }}
+        >
+          <PostWidget
+            slug={post.slug}
+            categories={post.categories.map((category) => category.slug)}
+          />
+          <Categories />
+        </div>
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug);
+
+  return {
+    props: { post: data },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getPosts();
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: false,
+  };
 }
