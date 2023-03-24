1a73581f8e9decfb41dd0f832b4f74c845ddb133
alirezayoom <alirezayoom@gmail.com>
2023-03-24 16:10:19 +0430

    getCategories added

--- services/index.js
+++ services/index.js
@@ -78,3 +78,16 @@ export const getSimilarPosts = async () => {
   const result = await request(graphqlAPI, query);
   return result.posts;
 };
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.categories;
+};
