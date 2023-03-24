2a049b4c91dd405ed13f51ff5fc22c38d3fe8ff4
alirezayoom <alirezayoom@gmail.com>
2023-03-24 15:45:14 +0430

    class removed

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -74,7 +74,6 @@ const PostCard = ({ post }) => {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-calendar-check"
               viewBox="0 0 16 16"
             >
               <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
