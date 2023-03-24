3d6bf8d1be90b384049fb814708cc31e54b0485b
alirezayoom <alirezayoom@gmail.com>
2023-03-24 13:18:36 +0430

    style added

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -30,53 +30,56 @@ const PostCard = ({ post }) => {
         <h1 style={{ fontSize: "1.5rem", textAlign: "center" }}>
           <Link href={`/post/${post.slug}`}>{post.title}</Link>
         </h1>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "0.625rem",
-          }}
-        >
+        {/* author and pulish date */}
+        <div style={{ display: "flex", gap: "1rem", justifyContent: "center" }}>
           <div
             style={{
-              position: "relative",
-              width: "30px",
-              aspectRatio: "1/1",
-              overflow: "hidden",
-              borderRadius: "9rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "0.625rem",
             }}
           >
-            <Image
-              src={post.author.avatar.url}
-              width={100}
-              height={100}
-              alt={post.author.name}
-              style={{ position: "absolute", width: "100%", height: "100%" }}
-            />
+            <div
+              style={{
+                position: "relative",
+                width: "30px",
+                aspectRatio: "1/1",
+                overflow: "hidden",
+                borderRadius: "9rem",
+              }}
+            >
+              <Image
+                src={post.author.avatar.url}
+                width={100}
+                height={100}
+                alt={post.author.name}
+                style={{ position: "absolute", width: "100%", height: "100%" }}
+              />
+            </div>
+            <div style={{}}>{post.author.name}</div>
           </div>
-          <div style={{}}>{post.author.name}</div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "0.625rem",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-calendar-check"
-            viewBox="0 0 16 16"
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "0.625rem",
+            }}
           >
-            <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
-            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-          </svg>
-          <div>{moment(post.createdAt).format("DD MMM YYYY")}</div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-calendar-check"
+              viewBox="0 0 16 16"
+            >
+              <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+            </svg>
+            <div>{moment(post.createdAt).format("DD MMM YYYY")}</div>
+          </div>
         </div>
         <div style={{ fontSize: "1.125rem", textAlign: "center" }}>
           {post.excerpt}
