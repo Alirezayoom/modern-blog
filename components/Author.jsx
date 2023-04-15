60aa28901db54a3a40a4d875e336f41631ff63ea
alirezayoom <alirezayoom@gmail.com>
2023-04-15 09:55:51 +0430

    author added

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,5 +1,52 @@
-const Author = () => {
-  return <div>Author</div>;
+import Image from "next/image";
+
+const Author = ({ author }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#333",
+        marginTop: "2rem",
+        borderRadius: "1rem",
+      }}
+    >
+      <div
+        style={{
+          width: "60px",
+          aspectRatio: "1/1",
+          position: "relative",
+          borderRadius: "9rem",
+          overflow: "hidden",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "-30px",
+        }}
+      >
+        <Image
+          src={author.avatar.url}
+          width={60}
+          height={60}
+          alt={author.name}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+          }}
+        />
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gap: "0.625rem",
+          textAlign: "center",
+          margin: "1rem",
+          marginBottom: "1.5rem",
+        }}
+      >
+        <h3>{author.name}</h3>
+        <p>{author.bio}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Author;
