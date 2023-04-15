3eff4183d360c4ce8b7230371b1f925b3883e53d
alirezayoom <alirezayoom@gmail.com>
2023-04-15 09:25:39 +0430

    getContentFragment added

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,7 +1,65 @@
+import React from "react";
 import Image from "next/image";
 import moment from "moment/moment";
 
 const PostDetail = ({ post }) => {
+  const getContentFragment = (index, text, obj, type) => {
+    let modifiedText = text;
+
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = <b key={index}>{text}</b>;
+      }
+
+      if (obj.italic) {
+        modifiedText = <em key={index}>{text}</em>;
+      }
+
+      if (obj.underline) {
+        modifiedText = <u key={index}>{text}</u>;
+      }
+    }
+
+    switch (type) {
+      case "heading-three":
+        return (
+          <h3 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        );
+      case "paragraph":
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        );
+      case "heading-four":
+        return (
+          <h4 key={index} className="text-md font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        );
+      case "image":
+        return (
+          <Image
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        );
+      default:
+        return modifiedText;
+    }
+  };
+
   return (
     <div
       style={{
@@ -21,7 +79,7 @@ const PostDetail = ({ post }) => {
       </div>
 
       {/* author and date */}
-      <div style={{ padding: "1rem" }}>
+      <div style={{ padding: "1.5rem" }}>
         <div
           style={{
             display: "flex",
@@ -82,6 +140,14 @@ const PostDetail = ({ post }) => {
         <h1 style={{ textAlign: "center", marginBottom: "1rem" }}>
           {post.title}
         </h1>
+        {/* {console.log(post.content.raw)} */}
+        {post.content.raw.children.map((typeObj, index) => {
+          const children = typeObj.children.map((item, itemindex) =>
+            getContentFragment(itemindex, item.text, item)
+          );
+
+          return getContentFragment(index, children, typeObj, typeObj.type);
+        })}
       </div>
     </div>
   );
