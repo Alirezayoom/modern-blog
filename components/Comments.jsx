e3ee8e5a88a3b4763867bcb82a6f05b782d229e7
alirezayoom <alirezayoom@gmail.com>
2023-04-16 17:50:19 +0430

    comments logic added

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,5 +1,33 @@
-const Comments = () => {
-  return <div>Comments</div>;
+import { useState, useEffect } from "react";
+import moment from "moment/moment";
+import parse from "html-react-parser";
+
+import { getComments } from "../services";
+import { comment } from "postcss";
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result));
+  }, [slug]);
+
+  return (
+    <div>
+      {comments.length > 0 && (
+        <div>
+          <h3>{comments.length} Comments</h3>
+          {comments.map((comment) => (
+            <div key={comment.createdAt}>
+              <p>{comment.name}</p> on{" "}
+              {moment(comment.createdAt).format("DD MMM YYYY")}
+              <p>{parse(comment.comment)}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Comments;
